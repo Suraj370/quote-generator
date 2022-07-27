1e986318984c51640e4555dda87453f30e1781e3
Suraj370 <surajpanda2077@gmail.com>
2022-07-27 22:00:19 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
 
 function App() {
+  const [quote, setQuote] = useState("");
+  const [author, SetAuthor] = useState("");
+
+const quoteAPI = async() =>{
+  let quotelist = [];
+  try{
+    const data = await axios.get("https://api.quotable.io/random");
+    quotelist = data.data;
+    console.log(quotelist);
+  }
+  catch(err){
+    console.log(err);
+  }
+  try{
+    setQuote(quotelist.content);
+    SetAuthor(quotelist.author);
+
+  }
+  catch(err){
+    console.log(err);
+  }
+}
+
+useEffect(() => {
+quoteAPI();
+},[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+       <div className ='quote-wrapper'>
+        <h2>Quote of the Day</h2>
+        <small id = 'quote-tags'>Famous-Quotes</small>
+        <blockquote id = 'quote-text'>{quote}</blockquote>
+        <cite id = 'quote-author'>{author}</cite>
+        <button onClick={quoteAPI} type = 'button' id = 'gen-quote-btn'>Get new Quote</button>
+   
+ 
+       </div>
+    </header>
+  </div>
   );
 }
 
